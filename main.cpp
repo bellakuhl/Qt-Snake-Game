99e47eab4f84a1b59e3eb67b0065e9c7e9ddbe0b
Isabella Kuhl <iamkuhl@bu.edu>
2020-04-24 13:09:16 -0400

    Added arrow key functionality

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
-#include <QtGui>
+#include <QApplication>
+#include <QGraphicsScene>
+#include "snake.h"
+#include <QGraphicsView>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QWidget window;
-    window.resize(320, 240);
-    window.show();
-    window.setWindowTitle(
-        QApplication::translate("toplevel", "Top-level widget"));
+    QGraphicsScene * scene = new QGraphicsScene();
+    // create rect item to put in scene
+    snake * rect = new snake();
+    rect->setRect(0,0,50,50);
+    // add item to scene
+    scene->addItem(rect);
+    // make rect focusable
+    rect->setFlag(QGraphicsItem::ItemIsFocusable);
+    rect->setFocus();
+    // add view to scene
+    QGraphicsView * view = new QGraphicsView(scene);
+    view->show();
     return app.exec();
 }
