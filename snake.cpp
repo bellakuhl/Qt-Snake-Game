12dab0c366b4694c10bc0e703367567db37273f5
Isabella Kuhl <iamkuhl@bu.edu>
2020-04-26 21:01:00 -0400

    Added rotations with arrow key press

--- snake.cpp
+++ snake.cpp
@@ -3,12 +3,15 @@
 #include <QGraphicsScene>
 #include "food.h"
 #include <QTimer>
+#include <QTransform>
+#include <iostream>
+#include <string>
 
 snake::snake() {
   // connect
   QTimer * timer = new QTimer();
   connect(timer,SIGNAL(timeout()),this,SLOT(move()));
-  timer->start(50);
+  timer->start(30);
 }
 
 snake::~snake() {
@@ -16,16 +19,39 @@ snake::~snake() {
 }
 
 void snake::keyPressEvent(QKeyEvent * event) {
+  QRectF rect;
+  foreach(QGraphicsItem* item, scene()->selectedItems()) {
+    rect |= item->mapToScene(item->boundingRect()).boundingRect();
+  }
+  QPointF center = rect.center();
+  QTransform t;
+  t.rotate(90); // rotate 90 degrees
+  t.translate(-center.x(), -center.y()); // rotate around item center
+
   if (event->key() == Qt::Key_Left) {
     if (pos().x() > 0)
-    setPos(x()-10, y());
+    //setPos(x()-10, y());
+    setRotation(rotation() + 90);
+    if (!this->direction.compare("left")) { // if snake is already going left, it is now moving down
+      this->direction = "down";
+    } else if (!this->direction.compare("down")) {
+      this->direction = "right";
+    } else if (!this->direction.compare("right")) {
+      this->direction = "up";
+    }
   } else if (event->key() == Qt::Key_Right) {
-      if (pos().x() + 5 < 800)
+      if (pos().x() + 5 < 700) // check that snake is within screen bounds
       setPos(x()+10, y());
+      setRotation(rotation() - 90);
+      this->direction = "right";
   } else if (event->key() == Qt::Key_Up) {
+      if (pos().y() < 700)
       setPos(x(), y()-10);
+      this->direction = "up";
   } else if (event->key() == Qt::Key_Down) {
-      setPos(x(), y()+10);
+     if (pos().y() > 0)
+        setPos(x(), y()+10);
+     this->direction = "down";
   } else if (event->key() == Qt::Key_Space) {
     food * newfood = new food();
     scene()->addItem(newfood);
@@ -33,6 +59,13 @@ void snake::keyPressEvent(QKeyEvent * event) {
 }
 
 void snake::move() {
-  if (pos().y() > 0)
-  setPos(x(), y()-5);
+  if (!this->direction.compare("up")) { // if direction is up
+    setPos(x(), y()-1);
+  } else if (!this->direction.compare("down")) {
+    setPos(x(), y()+1);
+  } else if (!this->direction.compare("left")) {
+    setPos(x()-1,y());
+  } else if (!this->direction.compare("right")) {
+    setPos(x()+1,y());
+  }
 }
--- snake.h
+++ snake.h
@@ -3,12 +3,17 @@
 
 #include <QGraphicsRectItem>
 #include <QObject>
+#include <iostream>
+#include <string>
+using namespace std;
 
 class snake: public QObject, public QGraphicsRectItem {
   Q_OBJECT // required Qobject macro
 public:
   snake();
   ~snake();
+  string direction = "up";
+  bool rotated = false;
   void keyPressEvent(QKeyEvent * event);
 public slots:
   void move();
