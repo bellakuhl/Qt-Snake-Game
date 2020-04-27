9fbe1532c726e3b7c40fa93cda4e29aeaab9fe75
Isabella Kuhl <iamkuhl@bu.edu>
2020-04-26 21:01:19 -0400

    Added random food spawn timer

--- main.cpp
+++ main.cpp
@@ -3,25 +3,32 @@
 #include "snake.h"
 #include "food.h"
 #include <QGraphicsView>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     QGraphicsScene * scene = new QGraphicsScene();
     // create rect item to put in scene
-    snake * rect = new snake();
-    food * startFood = new food();
-    rect->setRect(0,0,5,10);
-    scene->addItem(rect);
-    scene->addItem(startFood);
+    snake * newSnake = new snake();
+    food * newFood = new food();
+    newSnake->setRect(0,0,5,10);
+    scene->addItem(newSnake);
+    scene->setSceneRect(0,0,700,700);
     // make rect focusable
-    rect->setFlag(QGraphicsItem::ItemIsFocusable);
-    rect->setFocus();
+    newSnake->setFlag(QGraphicsItem::ItemIsFocusable);
+    newSnake->setFocus();
     // add view to scene
     QGraphicsView * view = new QGraphicsView(scene);
     view->setFixedSize(700, 700);
     view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view->show();
+    newSnake->setPos(view->width()/2,view->height() - newSnake->rect().height());
+
+    // spawn food randomly
+    QTimer * timer = new QTimer();
+    QObject::connect(timer,SIGNAL(timeout()),newFood,SLOT(spawn()));
+    timer->start(2000);
     return app.exec();
 }
