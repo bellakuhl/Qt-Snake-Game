f4180f62e45821cf6a3ccb65d3ff8884d42daa5a
Isabella Kuhl <iamkuhl@bu.edu>
2020-04-28 19:39:34 -0400

    Readded spawning

--- food.cpp
+++ food.cpp
@@ -2,10 +2,12 @@
 #include <QApplication>
 #include <QGraphicsScene>
 #include <QGraphicsView>
+#include <QTimer>
 #include <QList>
 #include <iostream>
 
 food::food(){
+  count = 0;
   int xpos, ypos;
   xpos = (int)(qrand() % 600);
   ypos = (int)(qrand() % 600);
@@ -14,3 +16,11 @@ food::food(){
 }
 
 food::~food() {}
+
+void food::spawn() {
+  if (this->count < 4) { // only make new food it there are less than 4 on the screen
+    food * newFood = new food();
+    scene()->addItem(newFood);
+    this->count++;
+  }
+}
