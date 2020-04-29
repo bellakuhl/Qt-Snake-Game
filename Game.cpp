9d92635d66796d82a402474c5c7357d5ee81297d
Isabella Kuhl <iamkuhl@bu.edu>
2020-04-29 09:34:02 -0400

    Made game widget

---
+++ Game.cpp
@@ -0,0 +1,83 @@
+#include "Game.h"
+#include <QTimer>
+#include <QGraphicsTextItem>
+#include <qmessagebox.h>
+#include <QIcon>
+#include <QPushButton>
+#include <QFont>
+#include "snake.h"
+#include "wall.h"
+
+Game::Game(QWidget *parent){
+    // create the scene
+    scene = new QGraphicsScene();
+    scene->setSceneRect(0,0,700,700); // make the scene 800x600 instead of infinity by infinity (default)
+
+    // make the newly created scene the scene to visualize (since Game is a QGraphicsView Widget,
+    // it can be used to visualize scenes)
+    setScene(scene);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setFixedSize(700,700);
+
+    // create the snake
+    newSnake = new snake();
+    newSnake->setRect(0,0,10,10); // change the rect from 0x0 (default) to 10x10 pixels
+    newSnake->setPos(350,350); // start in the middle bottom of screen
+    // make the player focusable and set it to be the current focus
+    newSnake->setFlag(QGraphicsItem::ItemIsFocusable);
+    newSnake->setFocus();
+    // add the player to the scene
+    scene->addItem(newSnake);
+
+    Wall * top = new Wall();
+    top->setRect(0,0,700,20); // top part of wall
+    scene->addItem(top);
+    Wall * left = new Wall();
+    left->setRect(0,0,20,700); // left part of wall
+    scene->addItem(left);
+    Wall * right = new Wall();
+    right->setRect(680,0,20,700); // right part of wall
+    scene->addItem(right);
+    Wall * bottom = new Wall();
+    bottom->setRect(0,680,700,20); // bottom part of wall
+    scene->addItem(bottom);
+
+    // create the food
+    for (int i = 0; i < 3; i++) {
+      food * newFood = new food();
+      scene->addItem(newFood);
+    }
+    displayWelcome();
+    show();
+}
+
+void Game::displayWelcome() {
+  QMessageBox msgBox;
+  msgBox.setText("Welcome to Qt Snake");
+  msgBox.setInformativeText("Would you like to play?");
+  QPushButton *connectButton = msgBox.addButton("YES!", QMessageBox::ActionRole);
+  QPushButton *abortButton = msgBox.addButton("Nah", QMessageBox::ActionRole);
+
+  msgBox.exec();
+
+  if (msgBox.clickedButton() == connectButton) {
+      // play
+      return;
+  } else if (msgBox.clickedButton() == abortButton) {
+    QMessageBox msgBox1;
+    here:
+    msgBox1.setText("Are you sure?");
+    msgBox1.setInformativeText("It's really fun!");
+    msgBox1.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+    msgBox1.setDefaultButton(QMessageBox::No);
+    int ret = msgBox1.exec();
+    switch (ret) {
+    case QMessageBox::No:
+        break;
+    case QMessageBox::Yes:
+        goto here;
+    }
+  }
+  return;
+}
