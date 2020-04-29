4d88b69385a20461da9410f785219dc0648bcaeb
Isabella Kuhl <iamkuhl@bu.edu>
2020-04-29 19:07:28 -0400

    Added texture to food

--- food.cpp
+++ food.cpp
@@ -5,13 +5,22 @@
 #include <QTimer>
 #include <QList>
 #include <iostream>
+#include "Game.h"
 
-food::food(){
+extern Game * game;
+
+food::food(QGraphicsItem *parent): QObject(), QGraphicsPixmapItem(parent){
+  setPixmap(QPixmap(":/resources/images/covid.png"));
+  /*QPixmap bkgnd("/home/iamkuhl/EC535/ball.png");
+bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
+QPalette palette;
+palette.setBrush(QPalette::Background, bkgnd);
+this->setPalette(palette);*/
   count = 0;
   int xpos, ypos;
   xpos = (int)(qrand() % 600);
   ypos = (int)(qrand() % 600);
-  setRect(0,0,5,5);
+  //setRect(0,0,5,5);
   setPos(xpos,ypos);
 }
 
