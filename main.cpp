51db9eb3fe437955e2db2142e675cb3d6dc05d42
Isabella Kuhl <iamkuhl@bu.edu>
2020-04-29 09:52:33 -0400

    Updated main to use game widget

--- main.cpp
+++ main.cpp
@@ -1,47 +1,20 @@
 #include <QApplication>
-#include <QGraphicsScene>
-#include <QSignalMapper>
-#include "snake.h"
-#include "food.h"
-#include <QGraphicsView>
-#include <QTimer>
-#include "field.h"
-#include "snake.h"
-#include "wall.h"
-#include "piece.h"
-#include <QDebug>
-#include <qmessagebox.h>
-#include <qmenubar.h>
-#include <QIcon>
+#include "Game.h"
 
-void displayWelcome();
+Game * game;
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QGraphicsScene * scene = new QGraphicsScene();
-    // create rect item to put in scene
-    snake * newSnake = new snake();
-    Field * field = new Field();
-    newSnake->setRect(0,0,10,10);
-    scene->addItem(newSnake);
-    scene->addItem(field);
-
-    Wall * top = new Wall();
-    top->setRect(0,0,700,20); // top part of wall
-    scene->addItem(top);
-    Wall * left = new Wall();
-    left->setRect(0,0,20,700); // left part of wall
-    scene->addItem(left);
-    Wall * right = new Wall();
-    right->setRect(680,0,20,700); // right part of wall
-    scene->addItem(right);
-    Wall * bottom = new Wall();
-    bottom->setRect(0,680,700,20); // bottom part of wall
-    scene->addItem(bottom);
+    game = new Game();
+    game->show();
+    /*QGraphicsScene * scene = new QGraphicsScene();
+    displayWelcome();
 
     // make scene a 700x700 square
     scene->setSceneRect(0,0,700,700);
+    //scene->setPos(350,350);
+
     // make rect focusable
     newSnake->setFlag(QGraphicsItem::ItemIsFocusable);
     newSnake->setFocus();
@@ -55,33 +28,16 @@ int main(int argc, char *argv[])
     QPainter p(&bg);
     p.setBrush(QBrush(Qt::gray));
     p.drawRect(0, 0, 700, 700);
-
     view->setBackgroundBrush(QBrush(bg));
-    displayWelcome();
-    view->show();
 
-    QSignalMapper* signalMapper = new QSignalMapper (field) ;
+    view->setScene(scene);*/
+    //view->show();
+    /*QSignalMapper* signalMapper = new QSignalMapper (field) ;
     QTimer * timer = new QTimer();
     QObject::connect (timer, SIGNAL(timeout()), signalMapper, SLOT(map()));
     signalMapper -> setMapping (timer, newSnake->foodEaten) ;
     QObject::connect (signalMapper, SIGNAL(mapped(int)), field, SLOT(updateFood(int))) ;
-    timer->start(2000);
+    timer->start(2000);*/
 
     return app.exec();
-}
-
-void displayWelcome() {
-
-  // Display welcome message
-  QMessageBox msgBox;
-  msgBox.setText("Welcome to Qt Snake");
-  msgBox.setInformativeText("Would you like to play?");
-  msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::Yes);
-  msgBox.setDefaultButton(QMessageBox::Yes);
-  int ret = msgBox.exec();
-
-  switch (ret) {
-  case QMessageBox::Yes:
-      break;
-  }
 }
